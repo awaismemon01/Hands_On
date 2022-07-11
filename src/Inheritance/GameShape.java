a37b352ed5931714e18b6781ffc7791026f1c688
Mohammed Awais Memon <awaismemon2323@gmail.com>
2022-07-11 17:02:07 +0530

    Basic Inheritance

---
+++ src/Inheritance/GameShape.java
@@ -0,0 +1,7 @@
+package Inheritance;
+
+public class GameShape {
+    public void display(){
+        System.out.println("displaying shape");
+    }
+}
---
+++ src/Inheritance/TestShapes.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class TestShapes {
+    public static void main(String[] args) {
+        PlayerPiece shape = new PlayerPiece();
+        shape.display();
+        shape.movePiece();
+    }
+}
